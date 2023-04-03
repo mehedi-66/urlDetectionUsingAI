ec7440c0ef06a5567676b0e3cc4370968e8982d5
Mehedi Hasan <mehedisn3@gmail.com>
2023-04-03 23:47:32 +0600

    url detection

---
+++ basicConcepts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.Welcome, name='Welcome'),
+    path('url', views.Url, name='Url'),
+    path('testurl', views.TestUrl, name='TestUrl')
+]
